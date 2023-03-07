53dd6fd41ce06c3e2238dde149ccea774c037108
akshatcoder-hash <akshatcoder@outlook.com>
2023-03-08 01:39:49 +0530

    state of the program

---
+++ src/state.rs
@@ -0,0 +1,13 @@
+use borsh:: { BorshSeralize, BorshDeserialize }; use solana_program:: {     program_pack::{ IsInitialized, Sealed },
+    pubkey::Pubkey,
+    clock::UnixTimestamp
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Debug)] pub struct UserStakeInfo { pub is_initialized: bool, pub token_account: Pubkey, pub stake_start_time: UnixTimestamp, pub last_stake_redeem: UnixTimestamp, pub user_pubkey: Pubkey, pub stake_state: StakeState, }
+
+impl Sealed for UserStakeInfo { } impl IsInitialized for UserStakeInfo { fn is_initialized(&self) -> bool { self.is_initialized     }
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Debug, PartialEq)] pub enum StakeState {     Staked,
+    Unstaked
+}
