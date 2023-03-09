d874fc8dd1c87a14e91d815a94af17854c45a96e
akshatcoder-hash <akshatcoder@outlook.com>
2023-03-10 04:48:33 +0530

    userstakeinfo impl

--- src/processor.rs
+++ src/processor.rs
@@ -1,35 +1,49 @@
-use solana_program:: {     account_info:: { AccountInfo, next_account_info },
+use solana_program::{     account_info::{ AccountInfo, next_account_info },
     entrypoint::ProgramResult,
     pubkey::Pubkey,
+    msg,
+    sysvar::{ rent::Rent, Sysvar },
+    clock::Clock,
+    program_pack::IsInitialized,
+    system_instruction,
+    program::invoke_signed,
+    borsh::try_from_slice_unchecked,
+    program_error::ProgramError
 }
-use crate::instruction::StakeInstruction;
-pub fn process_instruction(     program_id: &Pubkey,
-    accounts: &[AccountInfo],
-instruction_data: &[u8] ) -> ProgramResult {
-let instruction = StakeInstruction::unpack(instruction_data)?;
-match instruction {         StakeInstruction::InitializeStakeAccount => process_initialize_stake_account(program_id, accounts),
-        StakeInstruction::Stake => process_stake(program_id, accounts),
-        StakeInstruction::Redeem => process_redeem(program_id, accounts),
-        StakeInstruction::Unstake => process_unstake(program_id, accounts)
-    }
-}
-
-accounts: &[AccountInfo]
+use borsh::BorshSerialize; use crate::instruction::StakeInstruction; use crate::error::StakeError; use create::state::{ UserStakeInfo, StakeState };
+fn process_initialize_stake_account(     program_id: &Pubkey,
+    accounts: &[AccountInfo]
 ) -> ProgramResult {
 let account_info_iter = &mut accounts.iter(); let user = next_account_info(account_info_iter)?; let nft_token = next_account_info(account_info_iter)?; let stake_state = next_account_info(account_info_iter)?; let system_program = next_account_info(account_info_iter)?;
-Ok(()) }
+let (stake_state_pda, bump_seed) = Pubkey::find_program_address(         &[user.key.as_ref(), nft_token_account.key.as_ref()],
+        program_id
+    );
 
-fn process_stake(     program_id: &Pubkey,
-    accounts: &[AccountInfo]
-) -> ProgramResult {
-Ok(()) }
+// Check to ensure that you're using the right PDA if stake_state_pda != *stake_state.key { msg!("Invalid seeds for PDA"); return Err(StakeError::InvalidPda.into());     }
 
-fn process_redeem(     program_id: &Pubkey,
-    accounts: &[AccountInfo]
-) -> ProgramResult {
-Ok(()) }
+let rent = Rent::get()?; let rent_lamports = rent.minimum_balance(UserStakeInfo::SIZE);
+msg!("Creating state account at {:?}", stake_state_pda);     invoke_signed(
+        &system_instruction::create_account(
+            user.key,
+            stake_state.key,
+            rent_lamports,
+            UserStakeInfo::SIZE.try_into().unwrap(),
+            program_id
+        ),
+        &[user.clone(), stake_state.clone(), system_program.clone()],
+        &[&[
+            user.key.as_ref(),
+            nft_token_account.key.as_ref(),
+            &[bump_seed],
+        ]],
+    )?;
 
-fn process_unstake(     program_id: &Pubkey,
-    accounts: &[AccountInfo]
-) -> ProgramResult {
+// Let's create account let mut account_data = try_from_slice_unchecked::<UserStakeInfo>(&stake_state.data.borrow()).unwrap()
+if account_data.is_initialized() { msg!("Account already initialized"); return Err(ProgramError::AccountAlreadyInitialized);     }
+
+    account_data.token_account = *nft_token_account.key;
+    account_data.user_pubkey = *user.key;
+    account_data.stake_state = StakeState::Unstaked;
+account_data.is_initialized = true;
+account_data.serialize(&mut &mut stake_state.data.borrow_mut()[..])?;
 Ok(()) }
--- src/state.rs
+++ src/state.rs
@@ -1,9 +1,15 @@
-use borsh:: { BorshSeralize, BorshDeserialize }; use solana_program:: {     program_pack::{ IsInitialized, Sealed },
+use borsh:: { BorshSeralize, BorshDeserialize }; 
+use solana_program:: {     
+    program_pack::{ IsInitialized, Sealed },
     pubkey::Pubkey,
     clock::UnixTimestamp
 }
 
 #[derive(BorshSerialize, BorshDeserialize, Debug)] pub struct UserStakeInfo { pub is_initialized: bool, pub token_account: Pubkey, pub stake_start_time: UnixTimestamp, pub last_stake_redeem: UnixTimestamp, pub user_pubkey: Pubkey, pub stake_state: StakeState, }
+
+impl UserStakeInfo { 
+     pub const SIZE: usize = 1 + 32 + 64 + 64 + 32 + 1; 
+    }
 
 impl Sealed for UserStakeInfo { } impl IsInitialized for UserStakeInfo { fn is_initialized(&self) -> bool { self.is_initialized     }
 }
