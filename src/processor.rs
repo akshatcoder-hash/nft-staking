bfd3641858cdeb78e88c0f13ef2b769ed8128edd
akshatcoder-hash <akshatcoder@outlook.com>
2023-03-11 13:33:14 +0530

    redeem function

--- src/processor.rs
+++ src/processor.rs
@@ -60,7 +60,7 @@ let (stake_state_pda, _bump_seed) = Pubkey::find_program_address(         &[user
 
 if stake_state_pda != *stake_state.key { msg!("Invalid seeds for PDA"); return Err(StakeError::InvalidPda.into());     }
 
-// Let's create account let mut account_data = try_from_slice_unchecked::<UserStakeInfo>(&stake_state.data.borrow()).unwrap() 
+let mut account_data = try_from_slice_unchecked::<UserStakeInfo>(&stake_state.data.borrow()).unwrap() 
 if !account_data.is_initialized() { msg!("Account not initialized"); return Err(ProgramError::UninitializedAccount.into());     }
 
 let clock = Clock::get()?; 
@@ -70,5 +70,32 @@ let clock = Clock::get()?;
     account_data.stake_start_time = clock.unix_timestamp;
     account_data.last_stake_redeem = clock.unix_timestamp;
 account_data.is_initialized = true; 
+account_data.serialize(&mut &mut stake_state.data.borrow_mut()[..])?; 
+Ok(()) }
+
+fn process_redeem(     program_id: &Pubkey,
+    accounts: &[AccountInfo]
+) -> ProgramResult {
+let account_info_iter = &mut accounts.iter(); let user = next_account_info(account_info_iter)?; let nft_token_account = next_account_info(account_info_iter)?; let stake_state = next_account_info(account_info_iter)?; 
+let (stake_state_pda, _bump_seed) = Pubkey::find_program_address(         &[user.key.as_ref(), nft_token_account.key.as_ref()],
+        program_id,
+    );
+
+if stake_state_pda != *stake_state.key { msg!("Invalid seeds for PDA"); return Err(StakeError::InvalidPda.into());     }
+
+// For verification, we need to make sure it's the right signer 
+if !user.is_signer { msg!("Missing required signature"); return Err(ProgramError::MissingrequiredSignature);     }
+
+let mut account_data = try_from_slice_unchecked::<UserStakeInfo>(&stake_state.data.borrow()).unwrap() 
+if !account_data.is_initialized() { msg!("Account not initialized"); return Err(ProgramError::UninitializedAccount.into());     }
+
+if account_data.stake_state != StakeState::Staked { msg!("Stake account is not staking anything"); return Err(ProgramError::InvalidArgument);     }
+
+if *user.key != account_data.user_pubkey { msg!("Incorrect stake account for user"); return Err(StakeError::InvalidStakeAccount.into());     }
+
+if *nft_token_account.key != account_data.token_account { msg!("NFT Token account do not match"); return Err(StakeError::InvalidTokenAccount.into())     }
+
+let clock = Clock::get()?; let unix_time = clock.unix_timestamp - account_data.last_stake_redeem; let redeem_amount = unix_time; msg!("Redeeming {} tokens", redeem_amount); 
+    account_data.last_stake_redeem = clock.unix_timestamp;
 account_data.serialize(&mut &mut stake_state.data.borrow_mut()[..])?; 
 Ok(()) }
